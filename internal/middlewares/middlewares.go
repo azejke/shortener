c3422dba2b411a4787e72ff27305de0e9b462d8d
Azamat Kuandykov <azejkee@gmail.com>
2024-03-05 21:28:32 +0500

    feat: iter8

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -19,18 +19,19 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 func GzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		contentType := r.Header.Get("Content-Type")
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") || contentType != "application/json" {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") && (contentType == "application/json" || contentType == "text/html") {
+			gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+			if err != nil {
+				io.WriteString(w, err.Error())
+				return
+			}
+			defer gz.Close()
+			w.Header().Set("Content-Encoding", "gzip")
+			next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+			return
+		} else {
 			next.ServeHTTP(w, r)
 			return
 		}
-
-		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-		defer gz.Close()
-		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 	})
 }
