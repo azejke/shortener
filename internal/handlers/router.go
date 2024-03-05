651ecdc46030d64cc5d0da810a11751dcb79678e
Azamat Kuandykov <azejkee@gmail.com>
2024-03-05 22:52:10 +0500

    feat: iter8

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -3,16 +3,17 @@ package handlers
 import (
 	"github.com/azejke/shortener/internal/config"
 	"github.com/azejke/shortener/internal/logger"
-	"github.com/azejke/shortener/internal/middlewares"
 	"github.com/azejke/shortener/internal/store"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func RoutesBuilder(cfg *config.Config, s *store.Store) chi.Router {
 	handlers := URLHandler{storage: s, cfg: cfg}
 	r := chi.NewRouter()
 	r.Use(logger.RequestLogger)
-	r.Use(middlewares.GzipHandle)
+	//r.Use(middlewares.GzipHandle)
+	r.Use(middleware.Compress(5))
 	r.Route("/api", func(r chi.Router) {
 		r.Post("/shorten", handlers.Shorten)
 	})
